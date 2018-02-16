b7fd06896527d8355735b82b92b607fb4542765c
Fekete Márk <fmark98@gmail.com>
2018-02-16 12:23:26 +0100

    Questions added.

---
+++ BitRace/BitRaceServer/QuestionTypes/MainQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BitRaceServer.Enums;
+
+namespace BitRaceServer.QuestionTypes
+{
+    class MainQuestion : Question
+    {
+        List<PrimaryExtensionQuestion> extensionQuestions = new List<PrimaryExtensionQuestion>();
+
+        public List<PrimaryExtensionQuestion> ExtensionQuestions
+        {
+            get { return new List<PrimaryExtensionQuestion>(extensionQuestions); }
+        }
+
+        public MainQuestion(int id, string text, Dictionary<char, string> optionalAnswers, List<PrimaryExtensionQuestion> extensionQuestions) : base(id, text, optionalAnswers)
+        {
+            this.extensionQuestions = extensionQuestions;
+            difficulty = Difficulty.hard;
+        }
+    }
+}
---
+++ BitRace/BitRaceServer/QuestionTypes/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BitRaceServer.Enums;
+
+namespace BitRaceServer
+{
+    abstract class Question
+    {
+        protected int id;
+        protected string text;
+        protected Difficulty difficulty;
+        protected Dictionary<char, string> optionalAnswers;
+        protected List<char> keysOfSelectedIncorrectAnsver = new List<char>();
+
+        public Question(int id, string text, Dictionary<char, string> optionalAnswers)
+        {
+            this.id = id;
+            this.text = text;
+            this.optionalAnswers = new Dictionary<char, string>(optionalAnswers);
+            this.keysOfSelectedIncorrectAnsver = new List<char>();
+        }
+
+        internal char keyOfCorrectAnsver()
+        {
+            return (char)0; // todo sql query
+        }
+    }
+}

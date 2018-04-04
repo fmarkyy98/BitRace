68d1353a0229b01c3f18ed203a3b1ed6825e7d98
Suli <Suli@gt209-454>
2018-04-04 10:42:33 +0200

    PlayerConnector Class almost Done.

--- BitRace/BitRaceServer/Connectors/PlayerConnector.cs
+++ BitRace/BitRaceServer/Connectors/PlayerConnector.cs
@@ -1,13 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BitRaceServer
 {
     class PlayerConnector
     {
-        PlayerConnector() { }
+        Socket connection;
+        string input;
+        string output;
+        bool isRead;
+        bool IsRead { get { return isRead; } }
+
+        public string Input
+        {
+            get
+            {
+                isRead = true;
+                return input;
+            }
+        }
+        public string Output { get { return output; } set { output = value; } }
+
+        public PlayerConnector(Socket socket)
+        {
+            this.connection = socket;
+            new Thread(listener).Start();
+        }
+        private void listener()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int recieveSize = connection.Receive(buffer);
+                    string input = Encoding.ASCII.GetString(buffer, 0, recieveSize);
+                    if (input != this.input)
+                    {
+                        isRead = false;
+                        this.input = input;
+                    }
+                }
+                catch { }
+            }
+        }
+
+        public void SendData()
+        {
+            byte[] buffer = new byte[1024];
+            int recieveSize = connection.Receive(buffer);
+            byte[] data = Encoding.ASCII.GetBytes(output);
+            connection.Send(data);
+        }
+
     }
 }
--- BitRace/BitRaceServer/Player.cs
+++ BitRace/BitRaceServer/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,16 +12,27 @@ namespace BitRaceServer
         int id;
         string name;
         int score;
+        int[] actualQuestion = { 0, 0, 0 };
+        PlayerConnector playerConnector;
 
         public int Id { get { return this.id; } set { this.id = value; } }
         public string Name { get { return this.name; } set { this.name = value; } }
         public int Score { get { return this.score; } set { this.score = value > -1 ? value : 0; } }
+        PlayerConnector PlayerConnector { get { return this.playerConnector; } set { this.playerConnector = this.playerConnector ?? value; } }
 
         public Player(int id, string name, int score = 0)
         {
             this.id = id;
             this.name = name;
             this.score = score;
+        }
+
+        public void Conect(Socket socket)
+        {
+            if (playerConnector == null)
+            {
+                playerConnector = new PlayerConnector(socket);
+            }
         }
     }
 }

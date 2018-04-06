1170e426176179a499f241f16dbc569dd5aac9b8
Fekete Márk <fmark98@gmail.com>
2018-04-06 09:16:39 +0200

    Connection between player and server are ready. New management by more thread.

--- BitRace/BitRaceServer/Controller.cs
+++ BitRace/BitRaceServer/Controller.cs
@@ -16,24 +16,86 @@ namespace BitRaceServer
         static Game game = new Game(10, 3, 3);
         static ConnectionState sqlState;
 
+        static List<Socket> clientsGlobal = new List<Socket>();
+
+        static void threadFunction()
+        {
+            // connectedek ellenorzese, majd a hibasakat
+            while (true)
+            {
+                List<Socket> clients;
+                lock (clientsGlobal)
+                {
+                    clients = clientsGlobal.Where(x => x.Connected).ToList();
+                    clientsGlobal.RemoveAll(x => !clients.Contains(x));
+                }
+
+                if (clients.Count == 0)
+                    continue;
+
+                List<Socket> error = new List<Socket>();
+                Socket.Select(clients, null, error, 1000);
+                foreach (var s in error)
+                {
+                    clients.Remove(s);
+                }
+
+                foreach (var client in clients)
+                {
+                    byte[] buffer = new byte[1024];
+                    client.ReceiveTimeout = 1000;
+                    int recieveSize = 0;
+                    try { recieveSize = client.Receive(buffer); }
+                    catch (SocketException ex)
+                    {
+                        continue;
+                    }
+                    if (recieveSize <= 0)
+                        continue;
+                    string input = Encoding.ASCII.GetString(buffer, 0, recieveSize);
+                    string[] splitedInput = input.Split(';');
+
+                    if (splitedInput[0] == "mssql")
+                    {
+                        client.Send(Encoding.ASCII.GetBytes(/*sqlState.ToString()*/"mssql;connected".ToLower()));
+                    }
+                    else if (splitedInput[0] == "register")
+                    {
+                        int indexOfCurrent = game.Players.Select(x => x.Name).ToList().IndexOf(splitedInput[0]);
+                        if (indexOfCurrent == -1)
+                        {
+                            game.AddPlayer(splitedInput[0], client);
+                        }
+                        else
+                        {
+                            game.Players[indexOfCurrent].Conect(client);
+                        }
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+
+
             Socket connection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             connection.Bind(new IPEndPoint(new IPAddress(new byte[] { 127, 0, 0, 1 }), 8912));
             connection.Listen(0);
-            try
-            {
-                MSSQLConnector.BuildConnection("string", "string", "string", "string");
-                Controller.sqlState = building;
-                Console.WriteLine("Connection was succesfully built.");
-                Controller.sqlState = connected;
-            }
-            catch (InvalidOperationException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Controller.sqlState = disconnected;
-            }
+            //try
+            //{
+            //    MSSQLConnector.BuildConnection("string", "string", "string", "string");
+            //    Controller.sqlState = building;
+            //    Console.WriteLine("Connection was succesfully built.");
+            //    Controller.sqlState = connected;
+            //}
+            //catch (InvalidOperationException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Controller.sqlState = disconnected;
+            //}
             Console.WriteLine("Started Listening at LocalHost:8912");
+            new Thread(threadFunction).Start();
             while (true)
             {
                 Socket incoming = connection.Accept();
@@ -41,26 +103,9 @@ namespace BitRaceServer
                 {
                     continue;
                 }
-                byte[] buffer = new byte[1024];
-                int recieveSize = incoming.Receive(buffer);
-                string input = Encoding.ASCII.GetString(buffer, 0, recieveSize);
-                string[] splitedInput = input.Split(';');
-
-                if (splitedInput[0] == "mssql")
+                lock (clientsGlobal)
                 {
-                    incoming.Send(Encoding.ASCII.GetBytes(/*sqlState.ToString()*/"mssql;connected".ToLower()));
-                }
-                else if (splitedInput[0] == "register")
-                {
-                    int indexOfCurrent = game.Players.Select(x => x.Name).ToList().IndexOf(splitedInput[0]);
-                    if (indexOfCurrent == -1)
-                    {
-                        game.AddPlayer(splitedInput[0], incoming);
-                    }
-                    else
-                    {
-                        game.Players[indexOfCurrent].Conect(incoming);
-                    }
+                    clientsGlobal.Add(incoming);
                 }
             }
         }
